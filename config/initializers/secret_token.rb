5f6e60ac14b6e6065fd9e9971c6698bbc241b36b
Nick McGuire <nick@nickmcguire.com>
2013-10-18 21:44:28 -0400

    Initial Rails Rumble Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StockShop::Application.config.secret_key_base = '7cacf5a0f41e13e24b973d529b993751d59187e678444c81bf192e909d969ddfa6845eb9d3bcb21be154b07363e8b9e0aa7d488c96d42832eb5f9e0d737a7762'
