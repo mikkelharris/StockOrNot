5eb0eeef5a025d825b6c696a2ecf1e1dcf0be241
Mikkel Harris <mikkelharris@Mikkels-MacBook-Pro.local>
2013-10-20 12:34:30 -0400

    Radial buttons added to change excahnge, new logo

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -10,12 +10,13 @@ class StocksController < ApplicationController
 	end
 
 	def create
-		stock_params[:stock_symbol] = stock_params[:stock_symbol].upcase
+		@stock = Stock.new(stock_params)
+		@stock.stock_symbol = @stock.stock_symbol.upcase
 
-		@stock = Stock.find_by(stock_symbol: stock_params[:stock_symbol])
+		@stock = Stock.find_by(stock_symbol: @stock.stock_symbol, stock_exchange: @stock.stock_exchange)
 		@stock = Stock.new(stock_params) if @stock.nil?
 
-		value = get_value_for_symbol(@stock.stock_symbol)
+		value = stock_details(@stock.stock_symbol, @stock.stock_exchange)
 		@stock_value = StockValue.find_by(value: value, created_at: today_range)
 		@stock_value = StockValue.new({stock: @stock, value: value}) if @stock_value.nil?
 
@@ -34,6 +35,6 @@ class StocksController < ApplicationController
     @stock = Stock.find(params[:id])
   end
 	def stock_params
-		params.require(:stock).permit(:stock_symbol)
+		params.require(:stock).permit(:stock_symbol, :stock_exchange)
 	end
 end
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -3,8 +3,8 @@ require 'csv'
 
 module StocksHelper
 	
-	def get_value_for_symbol(symbol)
-		url = "http://finance.yahoo.com/d/quotes.csv?s=#{symbol}&f=l1"
+	def stock_details(symbol, exchange)
+		url = "http://finance.yahoo.com/d/quotes.csv?s=#{symbol}.#{exchange}&f=l1"
 		csv = CSV.parse(open(url).read)
 		result = Array.new
 		csv.each do |row|
