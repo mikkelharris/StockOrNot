000d1963f91c5b2edbb3c27fb8aae0738edf3b27
Nick McGuire <nick@nickmcguire.com>
2013-10-20 12:25:18 -0400

    Changed image scraping to regex and added image to the decision page

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,53 +1,26 @@
 require 'mechanize'
 
 module ItemsHelper
+  Regex = {
+    "walmart.com" => /http:\/\/i.walmartimages.com\/i\/p\/.+/,
+    "frys.com" => /http:\/\/images.frys.com\/art\/product\/.+/,
+    "newegg.com" => /.+\/ProductImageCompressAll300\/.+/,
+    "radioshack.com" => /.+\/graphics\/product_images\/.+/
+  }
+  
   def get_image(item)
-    image = nil
-    page = agent.get(item.full_site)
-    
-    if item.base_site.eql? "walmart.com"
-      image = get_image_walmart page
-    elsif item.base_site.eql? "frys.com"
-      # image = get_image_frys page
-    elsif item.base_site.eql? "radioshack.com"
-      # image = get_image_radioshock page
+    if !item.full_site.nil?
+      page = agent.get(item.full_site)
+      scrape_image_with_regex page, Regex[item.base_site]
     end
-    
-    image
   end
   
-  def get_image_walmart(page)
-    find_image(page, '#mainImage')
-  end
-  
-  def get_image_frys(page)
-    #Need to implement
-    find_image(page, '')
-  end
-  
-  def get_image_radioshock(page)
-    #Need to implement
-    # find_image(page, '')
-    
-    images = page.links
-    Rails.logger.debug images
-    images.each do |img|
-      Rails.logger.debug img
-      # img.node.attributes
-    end
-    images.first.to_s
+  def scrape_image_with_regex(page, regex)
+    page.images_with(src: regex).first.to_s
   end
   
   private
   def agent
     @agent ||= Mechanize.new
-  end
-  
-  def find_image(page, id)
-    images = page.search("img#{id}").map {|node| Mechanize::Page::Image.new(node, page) }
-    # images.each do |img|
-      # Rails.logger.debug img.to_s
-    # end
-    images.first.to_s
   end
 end
